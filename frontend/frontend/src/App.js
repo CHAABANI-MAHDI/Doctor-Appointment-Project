5f7eec92b1249eddad4bad00dfb23971c8ac1281
MahdiChaabani <mahdira201909@gmail.com>
2026-02-06 03:01:41 +0100

    feat: restructure app routing and add new page components

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -4,9 +4,14 @@ import Register from "./components/register.jsx";
 import NavBar from "./components/navBar.jsx";
 import { Routes, Route } from "react-router-dom";
 import AddAppointment from "./pages/addAppointment.jsx";
-import AddDoctors from "./pages/addDoctors.jsx";
+import DoctorsPage from "./pages/DoctorsPage.jsx";
 import MyAppointments from "./pages/myAppointments.jsx";
-import AdminDashboard from "./pages/adminDashboard.jsx";
+import OverviewPage from "./pages/OverviewPage.jsx";
+import Appointments from "./pages/AppointmentsPage.jsx";
+import Departments from "./pages/DepartmentsPage.jsx";
+import UsersPage from "./pages/usersAPage.jsx";
+import ServicesPage from "./pages/servicesPage.jsx";
+import AboutUsPage from "./pages/aboutUsPage.jsx";
 
 function App() {
   return (
@@ -17,10 +22,15 @@ function App() {
         <Route index path="/" element={<Home />} />
         <Route index path="/login" element={<Login />} />
         <Route index path="/register" element={<Register />} />
+        <Route index path="/services" element={<ServicesPage />} />
+        <Route index path="/about-us" element={<AboutUsPage />} />
         <Route index path="/add-appointment" element={<AddAppointment />} />
         <Route index path="/my-appointments" element={<MyAppointments />} />
-        <Route index path="/add-doctor" element={<AddDoctors />} />
-        <Route index path="/admin-dashboard" element={<AdminDashboard />} />
+        <Route index path="/admin-overview" element={<OverviewPage />} />
+        <Route index path="/admin-doctors" element={<DoctorsPage />} />
+        <Route index path="/admin-appointments" element={<Appointments />} />
+        <Route index path="/admin-departments" element={<Departments />} />
+        <Route index path="/admin-users" element={<UsersPage />} />
       </Routes>
     </>
   );
